8baad06bb9f342ec5d0927cb454da41b0c4c90c1
kentusati <m.khvalei@gmail.com>
2022-11-13 21:07:39 +0300

    8&9 labs

---
+++ Laba_9/MyTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace MyTest
+{
+    public class Tests
+    {
+        [Test]
+        public void SizeTableClick()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://asia.venum.com/ru/";
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.XPath("/html/body/div[3]/footer/div/div[1]/div[3]/div/div/div/div/div[2]/div[2]/ul/li[3]/a")).Click();
+            driver.Close();
+        }
+    }
+}
